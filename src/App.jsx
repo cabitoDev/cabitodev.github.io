35da854be1edecf1fe89869a098acf7232f0de62
carlos99at <carlos.abia99@gmail.com>
2024-03-10 01:23:17 +0100

    feat: quoteapp first steps

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { React, useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [quote, setQuote] = useState()
+
+  const getRandomQuote = () => {
+    fetch('https://chiquitadas.es/api/quotes/avoleorrr')
+    .then(res=> 
+      res.json())
+    .then(res=> setQuote(res.quote))
+  }
+  useEffect(()=>{
+    getRandomQuote();
+  },[])
+
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='quote_box'>
+        <div className='qoute_text'>
+          {quote}
+        </div> 
+        <div className='quote_footer'>
+          <button>tw</button>
+          <button>tm</button>
+          <button onClick={getRandomQuote}>New quote</button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
