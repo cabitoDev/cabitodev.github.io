dd91a279ec529b97802eaf0193add10ba27ec4b9
carlos99at <carlos.abia99@gmail.com>
2024-03-10 11:28:03 +0100

    style: styles finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,71 @@
-import { React, useEffect, useState } from 'react'
-import './App.css'
+import { React, useEffect, useState } from "react";
+import "./App.css";
+import { motion } from "framer-motion";
 
 function App() {
-  const [quote, setQuote] = useState()
-  const [author, setAuthor] = useState()
+  const [quote, setQuote] = useState();
+  const [author, setAuthor] = useState();
+  const [animate, setAnimate] = useState({ opacity: 1, y: 0 });
 
   const requestOptions = {
-    method: 'GET',
+    method: "GET",
     headers: {
-      'X-Api-Key': 'ZeGPaQg9U82qmWBZ1tPRtQ==MQRnplX2kD1Bw9hd',
+      "X-Api-Key": "ZeGPaQg9U82qmWBZ1tPRtQ==MQRnplX2kD1Bw9hd",
     },
-    contentType: 'application/json',
+    contentType: "application/json",
   };
 
   const getRandomQuote = () => {
-    fetch('https://api.api-ninjas.com/v1/quotes?category', requestOptions)
-    .then(res=> 
-      res.json())
-    .then(res=> {
-      setQuote(res[0].quote);
-    setAuthor(res[0].author)})
-  }
-  useEffect(()=>{
+    setAnimate({ opacity: 0, y: 1 });
+    fetch("https://api.api-ninjas.com/v1/quotes?category", requestOptions)
+      .then((res) => res.json())
+      .then((res) => {
+        setQuote(res[0].quote);
+        setAuthor(res[0].author);
+        setAnimate({ opacity: 1, y: 0 });
+      });
+  };
+  useEffect(() => {
     getRandomQuote();
-  },[])
-
-  
+  }, []);
 
   return (
-    <>
-      <div className='quote'>
-        <div className='quote_box'>
-        <div className='qoute_text'>
-          {quote}
-        </div> 
-        <div className='qoute_author'>
-          {author}
-        </div> 
-        </div>
-        <div className='quote_footer'>
-          
-          <a href={`https://twitter.com/intent/post?hashtags=quotes&related=freecodecamp&text="${quote}" ${author}`} target="_blank" rel="noreferrer">
-          <button>Twit it</button>
-          </a>
-
-          <a href={`https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=${author}&content=${quote}&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button`} target="_blank" rel="noreferrer">
-          <button>Twit it</button>
-          </a>
-            
-          <button onClick={getRandomQuote}>New quote</button>
+      <div id="quote-box" className="quote">
+        <motion.div 
+          key={quote}
+          className="quote_box"
+          initial={{ opacity: 0, y: -20 }}
+          animate={animate}
+          transition={{ duration: 0.5 }}
+        >
+          <div id="text" className="quote_text">{quote}</div>
+          <div id="author" className="quote_author">{author}</div>
+        </motion.div>
+        <div className="quote_footer">
+          <div className="quote_social">
+            <a id="tweet-quote"
+              href={`https://twitter.com/intent/post?hashtags=quotes&related=freecodecamp&text="${quote}" ${author}`}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <button>
+                <img src="/src/assets/twitter.svg" />
+              </button>
+            </a>
+            <a
+              href={`https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption=${author}&content=${quote}&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button`}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <button>
+                <img src="/src/assets/tumblr.svg" />
+              </button>
+            </a>
+          </div>
+          <button id="new-quote" onClick={getRandomQuote}>New quote</button>
         </div>
       </div>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
